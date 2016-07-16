09e7d90d0addbf4cb4f0c87bba6062bc08bbdfdc
731613205025 <maniharish95@gmail.com>
2016-07-16 11:48:25 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fact;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class program {
+    private static int n;
+
+ 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       n=sc.nextInt();
+       int n=5;
+       int fact=n;
+       for(int i=(n-1);i>1;i--)
+       {
+           fact=fact*i;
+       }
+        System.out.println("fact of no is"+fact);
+       }
+    }
+
